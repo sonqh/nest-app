2a728083b6422ac0da8a239606f0bfc0b880b326
sonqh <quachhoanhson@gmail.com>
2024-03-09 17:47:28 +0700

    refactor: change export struct

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,3 +1,2 @@
-export * from './database/database.module';
-export * from './database/abstract.repository';
-export * from './database/abstract.schema';
+export * from './database';
+export * from './logger';
